20055742cc5b01ef249b1d6147e0d5fdf7b01b52
Veer <veer@192.168.1.5>
2022-11-03 01:42:57 +0530

    marketplace listing api added

--- api/controllers/NftController.js
+++ api/controllers/NftController.js
@@ -6,7 +6,10 @@
  */
 
 module.exports = {
-  
-
+    all:async (req,res) => {
+        await Nft.find().populateAll().then(result => {
+            res.status(200).json(result);
+          });
+      }
 };
 
