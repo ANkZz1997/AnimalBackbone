74962fb9c9ea5a40523ad8de98fdda235c6036b3
Raheel <ahdan.ahmad@sdnatech.com>
2023-02-02 17:19:21 +0530

    conversation update

--- api/controllers/ConversationController.js
+++ api/controllers/ConversationController.js
@@ -1,13 +1,14 @@
 module.exports = {
   index: (req, res) => {
     const {id, t} = req.query;
-    const criteria = {id, user: req.payload.id}
-    let promise = null;
+    let promise = Dispute.find({id, user: req.payload.id});
     if(t) {
-      promise = Dispute.find({id, user: req.payload.id});
-      promise.populate('conversation', {where: {createdAt: {'>': t}, response: true}})
+      if(req.payload.isAdmin) {
+        promise.populate('conversation', {where: {createdAt: {'>': t}, response: false}})
+      } else {
+        promise.populate('conversation', {where: {createdAt: {'>': t}, response: true}})
+      }
     } else {
-      promise = Dispute.find({id, user: req.payload.id});
       promise.populate('conversation')
     }
     promise.then(result => {
