85b9ec8ea08c2193b4862fb0b81a63d35ae926ea
Manoj Kumar <manoj.kumar@sdnatech.com>
2023-02-13 17:58:48 +0530

    activities apis

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -4,212 +4,294 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-const moment = require('moment');
+const moment = require("moment");
 module.exports = {
   users: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await User.count();
     const criteria = req.body;
     User.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .populate("user")
+      .populate("nft")
+      .populate("marketplace")
+      .populate("auction")
+      .populate("bid")
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .then(result => {
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   nft: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await Nft.count();
     const criteria = req.body;
 
     Nft.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .then(result => {
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
-
   },
   auctions: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await Auction.count();
     const criteria = req.body;
     Auction.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .populate('nft')
-      .then(result => {
+      .populate("nft")
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   marketplace: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await Marketplace.count();
     const criteria = req.body;
     Marketplace.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .populate('nft')
-      .then(result => {
+      .populate("nft")
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   bids: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC', auction = null} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+      auction = null,
+    } = req.query;
     const criteria = req.body;
-    if(auction){
-      criteria['auction'] = auction;
+    if (auction) {
+      criteria["auction"] = auction;
     }
     const totalCount = await Bid.count(criteria);
     Bid.find(criteria)
       .limit(limit)
       .populate("user")
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
       .populate("auction.nft")
-      .then(result => {
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   dashboard: async (req, res) => {
-    const startOfDay = moment().startOf('day').valueOf();
+    const startOfDay = moment().startOf("day").valueOf();
     const totalUsers = await User.count();
-    const activeUserCount = await User.count({status: 'ACTIVE'});
-    const blockedUserCount = await User.count({status: 'BLOCKED'});
-    const newUserCount = await User.count({status: 'NEW'});
-    const todayUser = await User.count({createdAt: {'>=': startOfDay}});
-    const inactiveUser = await User.count({status: 'INACTIVE'});
+    const activeUserCount = await User.count({ status: "ACTIVE" });
+    const blockedUserCount = await User.count({ status: "BLOCKED" });
+    const newUserCount = await User.count({ status: "NEW" });
+    const todayUser = await User.count({ createdAt: { ">=": startOfDay } });
+    const inactiveUser = await User.count({ status: "INACTIVE" });
     res.ok({
-      totalUsers, activeUserCount, newUserCount, todayUser, inactiveUser, blockedUserCount
+      totalUsers,
+      activeUserCount,
+      newUserCount,
+      todayUser,
+      inactiveUser,
+      blockedUserCount,
     });
   },
   updateUserStatus: (req, res) => {
     const { id, status } = req.body;
-    User.update({id}).set({status})
+    User.update({ id })
+      .set({ status })
       .fetch()
-      .then(result => {
+      .then((result) => {
         res.ok(result);
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   getUserDetail: (req, res) => {
-    const {id} = req.query;
-    User.findOne({id})
+    const { id } = req.query;
+    User.findOne({ id })
       .populateAll()
-      .then(result => {
+      .then((result) => {
         res.ok(result);
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   getMarketplaceDetail: (req, res) => {
-    const {id} = req.query;
-    Marketplace.findOne({id})
+    const { id } = req.query;
+    Marketplace.findOne({ id })
       .populateAll()
-      .then(result => {
+      .then((result) => {
         res.ok(result);
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   getAuctionDetail: (req, res) => {
-    const {id} = req.query;
-    Auction.findOne({id})
+    const { id } = req.query;
+    Auction.findOne({ id })
       .populateAll()
-      .then(result => {
+      .then((result) => {
         res.ok(result);
-      }).catch(e => {
+      })
+      .catch((e) => {
         res.badRequest(e);
       });
   },
   getBidDetail: (req, res) => {
-    const {id} = req.query;
-    Bid.findOne({id})
+    const { id } = req.query;
+    Bid.findOne({ id })
       .populateAll()
-      .then(result => {
+      .then((result) => {
         res.ok(result);
-      }).catch(e => {
-        res.badRequest(e);
       })
+      .catch((e) => {
+        res.badRequest(e);
+      });
   },
   getNftDetail: (req, res) => {
-    const {id} = req.query
-    Nft.findOne({id})
+    const { id } = req.query;
+    Nft.findOne({ id })
       .populateAll()
-      .then(result => {
-        res.ok(result)
-      }).catch(e => {
-        res.badRequest(e)
-    })
+      .then((result) => {
+        res.ok(result);
+      })
+      .catch((e) => {
+        res.badRequest(e);
+      });
   },
   dispute: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await Dispute.count();
     const criteria = req.body;
 
     Dispute.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .then(result => {
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
-      res.badRequest(e);
-    });
-
+      })
+      .catch((e) => {
+        res.badRequest(e);
+      });
   },
   kyc: async (req, res) => {
-    const {page = 1, limit = 20, sort = 'createdAt', order = 'DESC'} = req.query;
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
     const totalCount = await Kyc.count();
     const criteria = req.body;
 
     Kyc.find(criteria)
       .limit(limit)
-      .skip((page-1)*limit)
+      .skip((page - 1) * limit)
       .sort(`${sort} ${order}`)
-      .then(result => {
+      .then((result) => {
         res.ok({
           records: result,
-          totalCount
+          totalCount,
         });
-      }).catch(e => {
-      res.badRequest(e);
-    });
-
+      })
+      .catch((e) => {
+        res.badRequest(e);
+      });
   },
 
+  getActivities: async (req, res) => {
+    const {
+      page = 1,
+      limit = 20,
+      sort = "createdAt",
+      order = "DESC",
+    } = req.query;
+    const criteria = req.body;
+    const totalCount = await Activity.count(criteria);
+    Activity.find(criteria)
+      .populateAll()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .sort(`${sort} ${order}`)
+      .then(async (result) => {
+        const data = await sails.helpers.displayActivities(result);
+        console.log(data);
+        res.ok({
+          records: data,
+          totalCount,
+        });
+      })
+      .catch((e) => {
+        res.badRequest(e);
+    });
+  },
 };
-
--- api/controllers/BidController.js
+++ api/controllers/BidController.js
@@ -6,17 +6,28 @@
  */
 
 module.exports = {
-  create: (req, res) => {
+  create: async (req, res) => {
     const {auctionId, price} = req.body;
     Auction.findOne({id: auctionId, status: 'ACTIVE'})
-      .then(result => {
+      .then(async result => {
         if(!result) return res.badRequest('Auction does not exist');
         if(price < result.basePrice) return res.badRequest('Price can not be less then base price');
         Bid.create({
           user: req.payload.id,
           auction: auctionId,
           price
-        }).fetch().then(_result => {
+        }).fetch().then(async _result => {
+          await sails.helpers.captureActivities({
+            action:"NFT",
+            type:"BID",
+            user:req.payload.id,
+            nft:result.nft,
+            auction:auctionId,
+            bid:_result.id,
+            payload:{
+              ipAddress:req.ip
+            }
+          });
           res.ok(_result);
         }).catch(e => {
           res.badRequest(e)
