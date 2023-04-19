e36c778b64811b4d6e864725719ac39517c9e719
Raheel <ahdan.ahmad@sdnatech.com>
2023-04-19 18:07:43 +0530

    initiate transaction fix

--- api/controllers/NftTransactionController.js
+++ api/controllers/NftTransactionController.js
@@ -8,8 +8,8 @@
 module.exports = {
   initiate: async (req, res) => {
     const {fromId, toId, nftId, marketplaceId, auctionId} = req.body;
-    const from = await User.find({id: fromId}).populate('wallet');
-    const to = await User.find({id: toId}).populate('wallet');
+    const from = await User.findOne({id: fromId}).populate('wallet');
+    const to = await User.findOne({id: toId}).populate('wallet');
     NftTransaction.create({
       fromUser: from.id,
       fromAddress: from.wallet.address.toLowerCase(),
