db41166967780460b0044b85badb1f09bfdd94d3
Manoj Kumar <manoj.kumar@sdnatech.com>
2023-04-20 16:39:34 +0530

    welcome email on social login

--- api/helpers/add-update-user.js
+++ api/helpers/add-update-user.js
@@ -59,6 +59,12 @@ module.exports = {
               user: result.id
             }).fetch();
             result.wallet = updatedWallet;
+            const settings = await sails.helpers.fetchSettings();
+            const name = `${result.firstName} ${result.lastName}`;
+            const info = { ...settings, name:name };
+            sails.helpers.sendMail(result.email, templates.welcomeEmail.subject(name), '', templates.welcomeEmail.content(info)).then(r => {
+              sails.log.info('Sending welcome email');
+            });
             await Kyc.create({user: result.id}).then(_result => {sails.log.info(`User's Kyc record created`)});
             result.token = await sails.helpers.signToken({ id: result.id });
             await User.update({ id: result.id }).set({
