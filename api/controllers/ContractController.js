5747ff78612b2c1595c23c8a07e4e9461ca0ed11
Raheel <ahdan.ahmad@sdnatech.com>
2023-04-26 17:24:50 +0530

    withdraw

--- api/controllers/ContractController.js
+++ api/controllers/ContractController.js
@@ -1,18 +1,26 @@
-const networks = require('./../constants/networks')
+const abi = require('./../constants/networks').abi
 const Web3 = require('web3');
+const { ethers } = require('ethers');
 
 module.exports = {
-  withdrawEthers: (req, res) => {
+  withdrawEthers: async (req, res) => {
+    const user = await User.findOne({id: req.payload.id}).populate('wallet');
+    const network = await Network.findOne({chainId: req.payload.chainId});
+    const provider = new ethers.JsonRpcProvider(network.host);
+    const signer = new ethers.Wallet(user.wallet.privateKey, provider);
+    const contract = new ethers.Contract(network.address, abi, signer);
+    const tx = await contract.withdraw();
+    const receipt = await tx.wait();
+    return res.ok(receipt);
 
   },
   availableToWithdraw: async (req, res) => {
-    const settings = await Settings.findOne({uid: 1});
+    const user = await User.findOne({id: req.payload.id}).populate('wallet');
     const network = await Network.findOne({chainId: req.payload.chainId});
     const web3 = new Web3(network.host);
-    const address = web3.eth.accounts.privateKeyToAccount(settings.adminPrivateKey);
-    const abi = sails.config.custom.nftAbi;
-    const contract = new web3.eth.Contract(abi, network.address);
-    contract.methods.availableToWithdraw().call().then(balance => {
+    const account = web3.eth.accounts.privateKeyToAccount(user.wallet.privateKey);
+    const contract = new web3.eth.Contract(abi, network.address );
+    contract.methods.availableToWithdraw().call({from: account.address}).then(balance => {
       res.ok({balance});
     }).catch(e => res.badRequest(e));
   }
